3e4ded9886e647dae313c1af9bad89d9d2c7ed90
Alexander Avdeev <numcard@gildiya.pro>
2024-10-16 14:09:19 +0300

    master небольшой рефакторинг

--- site.js
+++ site.js
@@ -1,65 +1,52 @@
-// удаляем даунский прелоадер
-$('.preloader').remove();
+[
+    '.preloader', // бесполезный прелоадер
+    '.center_sub', // меню слева
+    '.pro', // промокод
+    '.logol', // лого
+    '.user-menu', // меню
+    '.header > .container', // шапка
+    '.version_fixed', // версия сайта
+    '.name_sidebar .hello_sidebar', // приветствие
+    '.block_open_cases', // розыгрыш
+    '.today_visit_block', // блок кто зашел
+    '.today_online', // задняя часть фотки кто онлайн
+    '.users_chat_online .bx-chevron-down', // чат
+    '.login_visit, .img_back_visit', // чат
+    '.block_t_u, .heading_block, #new_news, .row_events', // новости
+    '#servers + div', // топики на форуме
+].forEach(selector => $(selector).remove());
 
-// удаляем меню слева
-$('.center_sub').remove();
+[
+    '.search_user', // поиск пользователей
+    '.head_block', // блок под меню
+].forEach(selector => $(selector).parent().remove());
 
-// удаляем найти пользователя
-$('.search_user').parent().remove();
-
-// удаляем промокод
-$('.pro').remove();
-
-// удаляем лого говна
-$('.logol').remove();
-
-// удаляем дубль меню
-$('.user-menu').remove();
-
-// шапка пустая - удаляем
-$('.header > .container').remove();
-
-// мусор
-$('.version_fixed').remove();
 
 // свое меню
 $('.navTopItems').html(`
-        <a class="navTopItem" href="/"><i class="bx bxs-group"></i>Главная</a>
+    <a class="navTopItem" href="/"><i class="bx bxs-group"></i>Главная</a>
     <a class="navTopItem" href="/gcms.php"><i class="bx bxs-group"></i>Форум</a>
     <a class="navTopItem" href="/store"><i class="bx bxs-store"></i>Магазин</a>
     <a class="navTopItem" href="/stats"><i class="bx bx-chart"></i>Стата</a>
     <a class="navTopItem" href="/banlist"><i class="bx bx-block"></i>Баны</a>
     <a class="navTopItem" href="/muts_v2"><i class="bx bx-block"></i>Муты</a>
     <a class="navTopItem" href="/chatlog"><i class="bx bx-list-ul"></i>Чатлог</a>
-    <a class="navTopItem" href="/support"><i class="bx bxs-help-circle"></i>Тех. поддержка</a>`);
+    <a class="navTopItem" href="/support"><i class="bx bxs-help-circle"></i>Тех. поддержка</a>
+`);
 
-// блок под меню нахуй
-$('.head_block').parent().remove();
-
-// приветствие
-$('.name_sidebar .hello_sidebar').remove();
-
-// розыгрышы для даунов
-$('.block_open_cases').remove();
-
-// разве не похуй кто заходил?
-$('.today_visit_block').remove();
-
+// магия с переносом блока баланса туда-обратно (говнокод)
 $('.flex_balance_mp').appendTo('.flex_mini_user');
 $('.flex_mini_user').appendTo('.balance_mini_profile');
 $('.button_plus_purse').parent().remove();
 $('.flex_mini_user').appendTo('.info_navigation');
 $('.balance_mini_profile').remove();
 $('.flex_balance_mp').prependTo('.flex_mini_user');
-
 $('.flex_balance_mp').replaceWith('<a href="/purse" class="flex_balance_mp">' + $('.flex_balance_mp').html() + '</a>');
 
+// блок онлайна
 $('.flex_today').html('Онлайн');
-$('.today_online').remove();
 
-$('.users_chat_online .bx-chevron-down').remove();
-$('.login_visit, .img_back_visit').remove();
-
+// при наведение на игрока пишется ник + должность
 setTimeout(() => {
     $('.last_online').each((i, $image) => {
         const $parent = $($image).parents('a')[0];
@@ -71,8 +58,5 @@ setTimeout(() => {
     })
 }, 1000);
 
-$('.block_t_u, .heading_block, #new_news, .row_events').remove();
-
-$('#servers + div').remove();
-
+// фикс бага с онлайном, когда онлайн 0 - полоска заполнена
 $('progress[value="0"]').each((i, $item) => $item.value = 1);
